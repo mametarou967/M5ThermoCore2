4420d887d81cb99bed83332b127c72ac973a8077
mametarou967 <mametarou967@gmail.com>
2023-06-11 21:59:31 +0900

    気温が即時に反映されるように修正

--- M5ThermoCore2.ino
+++ M5ThermoCore2.ino
@@ -1,42 +1,118 @@
 #include <M5Core2.h>    // CORE2使用の場合有効
 // #include <M5Stack.h>       // M5Stack GRAY使用の場合有効
 // #include <M5StickCPlus.h>  // M5StickC Plus使用の場合有効
-
+// lovyan
 #define LGFX_AUTODETECT // 自動認識(D-duino-32 XS, PyBadgeはパネルID読取れないため自動認識の対象から外れているそうです)
 #define LGFX_USE_V1     // v1.0.0を有効に(v0からの移行期間の特別措置とのこと。書かない場合は旧v0系で動作)
 #include <LovyanGFX.hpp>          // lovyanGFXのヘッダを準備
 #include <LGFX_AUTODETECT.hpp>    // クラス"LGFX"を準備
+// SHT3x
+#include <Wire.h>
+#include "Adafruit_Sensor.h"
+#include <Adafruit_BMP280.h>
+#include "SHT3X.h"
+
+// lovyan
 static LGFX lcd;                  // LGFXのインスタンスを作成（クラスLGFXを使ってlcdコマンドでいろいろできるようにする）
 static LGFX_Sprite canvas(&lcd);  // スプライトを使う場合はLGFX_Spriteのインスタンスを作成
+#define TEMP_DISP_MAX 40
+#define TEMP_DISP_MIN 0
 
-// 変数設定
-int cnt;        // 本体ボタンON回数格納用
-int x = 0;      // x軸座標格納用
-uint16_t color; // 登録色格納用
+// SHT3x
+SHT3X sht30;
+Adafruit_BMP280 bme;
+float tmp = 0.0;
+
+void updateView()
+{
+  canvas.fillScreen(BLACK);         // 背景塗り潰し
+  canvas.setTextColor(WHITE);       // 文字色と背景を指定（文字色, 背景（省略可））
+
+  // 四角
+  canvas.drawRect(24, 40, 292, 160, WHITE);  // 四角（始点x,始点y,縦長さ,横長さ）
+
+  // 補助線:縦
+  canvas.drawFastHLine(24,  80 + 1, 292 - 2, lcd.color332(128, 128, 128));
+  canvas.drawFastHLine(24, 120 + 1, 292 - 2, lcd.color332(128, 128, 128));
+  canvas.drawFastHLine(24, 160 + 1, 292 - 2, lcd.color332(128, 128, 128));
+  // 凡例:縦
+  int temp_disp_baseH = (TEMP_DISP_MAX + TEMP_DISP_MIN) / 4;
+  canvas.setFont(&fonts::lgfxJapanGothic_16);     // ゴシック体（8,12,16,20,24,28,32,36,40）
+  canvas.setCursor(4,32);
+  canvas.printf("%2d\n",temp_disp_baseH * 4);
+  canvas.setCursor(4,72);
+  canvas.printf("%2d\n",temp_disp_baseH * 3);
+  canvas.setCursor(4,112);
+  canvas.printf("%2d\n",temp_disp_baseH * 2);
+  canvas.setCursor(4,152);
+  canvas.printf("%2d\n",temp_disp_baseH * 1);
+  canvas.setCursor(4,192);
+  canvas.printf("%2d\n",TEMP_DISP_MIN);
+
+  // 補助線:横
+  int temp_disp_baseV = 24;
+  canvas.drawFastVLine(24 + (temp_disp_baseV *  1) + 1, 40, 160 - 2, lcd.color332(128, 128, 128));
+  canvas.drawFastVLine(24 + (temp_disp_baseV *  3) + 1, 40, 160 - 2, lcd.color332(128, 128, 128));
+  canvas.drawFastVLine(24 + (temp_disp_baseV *  5) + 1, 40, 160 - 2, lcd.color332(128, 128, 128));
+  canvas.drawFastVLine(24 + (temp_disp_baseV *  7) + 1, 40, 160 - 2, lcd.color332(128, 128, 128));
+  canvas.drawFastVLine(24 + (temp_disp_baseV *  9) + 1, 40, 160 - 2, lcd.color332(128, 128, 128));
+  canvas.drawFastVLine(24 + (temp_disp_baseV * 11) + 1, 40, 160 - 2, lcd.color332(128, 128, 128));
+  // 凡例:横
+  canvas.setFont(&fonts::lgfxJapanGothic_16);     // ゴシック体（8,12,16,20,24,28,32,36,40）
+  canvas.setCursor(24 + (temp_disp_baseV *  1) - 4 ,200 + 2);
+  canvas.println("8");            // 表示内容をcanvasに準備
+  canvas.setCursor(24 + (temp_disp_baseV *  3) - 8 ,200 + 2);
+  canvas.println("10");            // 表示内容をcanvasに準備
+  canvas.setCursor(24 + (temp_disp_baseV *  5) - 8 ,200 + 2);
+  canvas.println("12");            // 表示内容をcanvasに準備
+  canvas.setCursor(24 + (temp_disp_baseV *  7) - 8 ,200 + 2);
+  canvas.println("14");            // 表示内容をcanvasに準備
+  canvas.setCursor(24 + (temp_disp_baseV *  9) - 8 ,200 + 2);
+  canvas.println("16");            // 表示内容をcanvasに準備
+  canvas.setCursor(24 + (temp_disp_baseV * 11) - 8 ,200 + 2);
+  canvas.println("18");            // 表示内容をcanvasに準備
+
+  // 具縦粋な値
+  canvas.setFont(&fonts::lgfxJapanGothic_24);     // ゴシック体（8,12,16,20,24,28,32,36,40）
+  canvas.setCursor(30,8);
+  canvas.print("時刻 12:34"); 
+  canvas.setCursor(180,8);
+  canvas.print("気温 ");
+  canvas.printf("%2.1f",tmp); 
+
+  canvas.pushSprite(0, 0);  // メモリ内に描画したcanvasを座標を指定して表示する
+}
+
+
 // 初期設定 -----------------------------------------
 void setup() {
   M5.begin();   // 本体初期化
   // LCD初期設定
   lcd.init();                 // LCD初期化
-  lcd.setRotation(2);         // 画面向き設定（0～3で設定、4～7は反転)　※CORE2、GRAYの場合
-  //lcd.setRotation(1);         // 画面向き設定（0～3で設定、4～7は反転)　※M5StickC Plusの場合
+  lcd.setRotation(1);         // 画面向き設定（0～3で設定、4～7は反転)　※CORE2、GRAYの場合
   canvas.setColorDepth(8);    // カラーモード設定（書かなければ初期値16bit。24bit（パネル性能によっては18bit）は対応していれば選択可）
                               // CORE2 GRAY のスプライトは16bit以上で表示されないため8bitに設定
   canvas.setTextWrap(false);  // 改行をしない（画面をはみ出す時自動改行する場合はtrue）
   canvas.setTextSize(1);      // 文字サイズ（倍率）
   canvas.createSprite(lcd.width(), lcd.height()); // canvasサイズ（メモリ描画領域）設定（画面サイズに設定）
 
-  canvas.fillScreen(BLACK);         // 背景塗り潰し
-  canvas.setTextColor(WHITE);       // 文字色と背景を指定（文字色, 背景（省略可））
+  Wire.begin();
+  
+  while (!bme.begin(0x76)){
+    Serial.println("Could not find a 0x76");
+  }
 
-  // 四角
-  canvas.drawRect(40, 20, 180, 280, WHITE);  // 四角（始点x,始点y,縦長さ,横長さ）
-
-  canvas.pushSprite(0, 0);  // メモリ内に描画したcanvasを座標を指定して表示する
+  updateView();
 }
 // メイン -----------------------------------------
 void loop() {
   M5.update();                      // 本体ボタン状態更新
+  
+  if(sht30.get()==0){
+    tmp = sht30.cTemp;
+  }
 
-  delay(100); // 遅延時間（ms）
+  updateView();
+
+  delay(1000); // 遅延時間（ms）
 }
